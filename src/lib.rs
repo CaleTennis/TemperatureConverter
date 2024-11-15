7b874bb69c182c6b2021e47093be95dd2c47f35f
Cale Tennis <c.a.tennis@wustl.edu>
2024-11-15 07:51:28 -0600

    Added Romer/Unit Tests, passes all tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // lib.rs
 
+// Constants
 pub const FAHRENHEIT_FREEZING_TEMP: f64 = 32.0;
 pub const CELSIUS_FREEZING_TEMP: f64 = 0.0;
 pub const KELVIN_FREEZING_TEMP: f64 = 273.15;
@@ -7,6 +8,7 @@ pub const RANKINE_FREEZING_TEMP: f64 = 491.67;
 pub const DELISLE_FREEZING_TEMP: f64 = 150.0;
 pub const NEWTON_FREEZING_TEMP: f64 = 0.0;
 pub const REAUMUR_FREEZING_TEMP: f64 = 0.0;
+pub const ROMER_FREEZING_TEMP: f64 = 7.5;
 
 pub const FAHRENHEIT_BOILING_TEMP: f64 = 212.0;
 pub const CELSIUS_BOILING_TEMP: f64 = 100.0;
@@ -15,10 +17,13 @@ pub const RANKINE_BOILING_TEMP: f64 = 671.67;
 pub const DELISLE_BOILING_TEMP: f64 = 0.0;
 pub const NEWTON_BOILING_TEMP: f64 = 33.0;
 pub const REAUMUR_BOILING_TEMP: f64 = 80.0;
+pub const ROMER_BOILING_TEMP: f64 = 60.0;
 
 const DEGREE_SYMBOL: &str = "\u{00B0}";
 const LOWERCASE_E_ACUTE_ACCENT: &str = "\u{00E9}";
+const LOWERCASE_O_WITH_SLASH: &str = "\u{00F8}";
 
+// Temperature enumeration and struct
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub enum TemperatureUnit {
     Fahrenheit,
@@ -28,20 +33,22 @@ pub enum TemperatureUnit {
     Delisle,
     Newton,
     Reaumur,
+    Romer,
 }
 
-pub const TEMP_OPTIONS: &str = "F/C/K/R/De/N/Re";
+pub const TEMP_OPTIONS: &str = "F/C/K/R/De/N/Re/Ro";
 
 impl TemperatureUnit {
     pub fn to_string(self) -> String {
         match self {
             TemperatureUnit::Fahrenheit => format!("{DEGREE_SYMBOL}F"),
-            TemperatureUnit::Celsius => format!("{DEGREE_SYMBOL}C"),
-            TemperatureUnit::Kelvin => format!("K"),
-            TemperatureUnit::Rankine => format!("{DEGREE_SYMBOL}R"),
-            TemperatureUnit::Delisle => format!("{DEGREE_SYMBOL}De"),
-            TemperatureUnit::Newton => format!("{DEGREE_SYMBOL}N"),
-            TemperatureUnit::Reaumur => format!("{DEGREE_SYMBOL}R{LOWERCASE_E_ACUTE_ACCENT}")
+            TemperatureUnit::Celsius    => format!("{DEGREE_SYMBOL}C"),
+            TemperatureUnit::Kelvin     => format!("K"),
+            TemperatureUnit::Rankine    => format!("{DEGREE_SYMBOL}R"),
+            TemperatureUnit::Delisle    => format!("{DEGREE_SYMBOL}De"),
+            TemperatureUnit::Newton     => format!("{DEGREE_SYMBOL}N"),
+            TemperatureUnit::Reaumur    => format!("{DEGREE_SYMBOL}R{LOWERCASE_E_ACUTE_ACCENT}"),
+            TemperatureUnit::Romer      => format!("{DEGREE_SYMBOL}R{LOWERCASE_O_WITH_SLASH}"),
         }
     }
 }
@@ -56,59 +63,80 @@ impl Temperature {
         Temperature { value, unit }
     }
 
+    // Main functionality of program
     pub fn convert_to(&self, target_unit: TemperatureUnit) -> Temperature {
         let value_in_target_unit = match (self.unit, target_unit) {
+            (TemperatureUnit::Fahrenheit, TemperatureUnit::Fahrenheit) => self.value,
             (TemperatureUnit::Fahrenheit, TemperatureUnit::Celsius) => (self.value - 32.0) / 1.8,
             (TemperatureUnit::Fahrenheit, TemperatureUnit::Kelvin) => ((self.value - 32.0) / 1.8) + 273.15,
             (TemperatureUnit::Fahrenheit, TemperatureUnit::Rankine) => self.value + 459.67,
             (TemperatureUnit::Fahrenheit, TemperatureUnit::Delisle) => ((212.0 - self.value) / 6.0) * 5.0,
             (TemperatureUnit::Fahrenheit, TemperatureUnit::Newton) => ((self.value - 32.0) / 60.0) * 11.0,
             (TemperatureUnit::Fahrenheit, TemperatureUnit::Reaumur) => ((self.value - 32.0) / 9.0) * 4.0,
+            (TemperatureUnit::Fahrenheit, TemperatureUnit::Romer) => (((self.value - 32.0) / 24.0) * 7.0) + 7.5,
 
+            (TemperatureUnit::Celsius, TemperatureUnit::Celsius) => self.value,
             (TemperatureUnit::Celsius, TemperatureUnit::Fahrenheit) => (self.value * 1.8) + 32.0,
             (TemperatureUnit::Celsius, TemperatureUnit::Kelvin) => self.value + 273.15,
             (TemperatureUnit::Celsius, TemperatureUnit::Rankine) => (self.value * 1.8) + 491.67,
             (TemperatureUnit::Celsius, TemperatureUnit::Delisle) => ((100.0 - self.value) / 2.0) * 3.0,
             (TemperatureUnit::Celsius, TemperatureUnit::Newton) => (self.value / 100.0) * 33.0,
             (TemperatureUnit::Celsius, TemperatureUnit::Reaumur) => (self.value / 5.0) * 4.0,
-
+            (TemperatureUnit::Celsius, TemperatureUnit::Romer) => ((self.value / 40.0) * 21.0) + 7.5,
+            
+            (TemperatureUnit::Kelvin, TemperatureUnit::Kelvin) => self.value,
             (TemperatureUnit::Kelvin, TemperatureUnit::Fahrenheit) => ((self.value - 273.15) * 1.8) + 32.0,
             (TemperatureUnit::Kelvin, TemperatureUnit::Celsius) => self.value - 273.15,
             (TemperatureUnit::Kelvin, TemperatureUnit::Rankine) =>  ((self.value - 273.15) * 1.8) + 491.67,
             (TemperatureUnit::Kelvin, TemperatureUnit::Delisle) => ((373.15 - self.value ) / 2.0) * 3.0,
             (TemperatureUnit::Kelvin, TemperatureUnit::Newton) => ((self.value - 273.15) / 100.0) * 33.0,
             (TemperatureUnit::Kelvin, TemperatureUnit::Reaumur) => (self.value - 273.15) * 0.8,
+            (TemperatureUnit::Kelvin, TemperatureUnit::Romer) => (((self.value - 273.15) / 40.0) * 21.0) + 7.5,
 
+            (TemperatureUnit::Rankine, TemperatureUnit::Rankine) => self.value,
             (TemperatureUnit::Rankine, TemperatureUnit::Fahrenheit) => self.value - 459.67,
             (TemperatureUnit::Rankine, TemperatureUnit::Celsius) => (self.value - 491.67) / 1.8,
             (TemperatureUnit::Rankine, TemperatureUnit::Kelvin) =>  ((self.value - 491.67) / 1.8) + 273.15,
             (TemperatureUnit::Rankine, TemperatureUnit::Delisle) => ((671.67 - self.value) / 6.0) * 5.0,
             (TemperatureUnit::Rankine, TemperatureUnit::Newton) => ((self.value - 491.67) / 60.0) * 11.0,
             (TemperatureUnit::Rankine, TemperatureUnit::Reaumur) => ((self.value - 491.67) / 9.0) * 4.0,
+            (TemperatureUnit::Rankine, TemperatureUnit::Romer) => (((self.value - 491.67) / 24.0) * 7.0) + 7.5,
 
+            (TemperatureUnit::Delisle, TemperatureUnit::Delisle) => self.value,
             (TemperatureUnit::Delisle, TemperatureUnit::Fahrenheit) => 212.0 - ((self.value / 5.0) * 6.0),
             (TemperatureUnit::Delisle, TemperatureUnit::Celsius) => 100.0 - ((self.value / 3.0) * 2.0),
             (TemperatureUnit::Delisle, TemperatureUnit::Kelvin) => 373.15 - ((self.value / 3.0) * 2.0),
             (TemperatureUnit::Delisle, TemperatureUnit::Rankine) => 671.67 - ((self.value / 5.0) * 6.0),
             (TemperatureUnit::Delisle, TemperatureUnit::Newton) => 33.0 - ((self.value / 50.0) * 11.0),
             (TemperatureUnit::Delisle, TemperatureUnit::Reaumur) => 80.0 - ((self.value / 15.0) * 8.0),
+            (TemperatureUnit::Delisle, TemperatureUnit::Romer) => 60.0 - ((self.value / 20.0) * 7.0),
 
+            (TemperatureUnit::Newton, TemperatureUnit::Newton) => self.value,
             (TemperatureUnit::Newton, TemperatureUnit::Fahrenheit) => ((self.value / 11.0) * 60.0) + 32.0,
             (TemperatureUnit::Newton, TemperatureUnit::Celsius) => (self.value / 33.0) * 100.0,
             (TemperatureUnit::Newton, TemperatureUnit::Kelvin) => ((self.value / 33.0) * 100.0) + 273.15,
             (TemperatureUnit::Newton, TemperatureUnit::Rankine) => ((self.value / 11.0) * 60.0) + 491.67,
             (TemperatureUnit::Newton, TemperatureUnit::Delisle) => ((33.0 - self.value) / 11.0) * 50.0,
             (TemperatureUnit::Newton, TemperatureUnit::Reaumur) => (self.value / 33.0) * 80.0,
+            (TemperatureUnit::Newton, TemperatureUnit::Romer) => ((self.value / 22.0) * 35.0) + 7.5,
 
+            (TemperatureUnit::Reaumur, TemperatureUnit::Reaumur) => self.value,
             (TemperatureUnit::Reaumur, TemperatureUnit::Fahrenheit) => (self.value * 2.25) + 32.0, // New for Reaumur
             (TemperatureUnit::Reaumur, TemperatureUnit::Celsius) => (self.value / 4.0) * 5.0,
             (TemperatureUnit::Reaumur, TemperatureUnit::Kelvin) => (self.value / 0.8) + 273.15, 
             (TemperatureUnit::Reaumur, TemperatureUnit::Rankine) => (self.value * 2.25) + 491.67, 
             (TemperatureUnit::Reaumur, TemperatureUnit::Delisle) => ((80.0 - self.value) / 8.0) * 15.0,
             (TemperatureUnit::Reaumur, TemperatureUnit::Newton) => (self.value / 80.0) * 33.0,
-
-            // To & from the same unit
-            (_, _) => self.value,
+            (TemperatureUnit::Reaumur, TemperatureUnit::Romer) => ((self.value / 32.0) * 21.0) + 7.5,
+            
+            (TemperatureUnit::Romer, TemperatureUnit::Romer) => self.value,
+            (TemperatureUnit::Romer, TemperatureUnit::Fahrenheit) => (((self.value - 7.5) / 7.0) * 24.0) + 32.0,
+            (TemperatureUnit::Romer, TemperatureUnit::Celsius) => ((self.value - 7.5) / 21.0) * 40.0,
+            (TemperatureUnit::Romer, TemperatureUnit::Kelvin) => (((self.value - 7.5) / 21.0) * 40.0) + 273.15,
+            (TemperatureUnit::Romer, TemperatureUnit::Rankine) => (((self.value - 7.5) / 7.0) * 24.0) + 491.67,
+            (TemperatureUnit::Romer, TemperatureUnit::Delisle) => ((60.0 - self.value) / 7.0) * 20.0,
+            (TemperatureUnit::Romer, TemperatureUnit::Newton) => ((self.value - 7.5) / 35.0) * 22.0,
+            (TemperatureUnit::Romer, TemperatureUnit::Reaumur) => ((self.value - 7.5) / 21.0) * 32.0,
         };
 
         if value_in_target_unit.is_infinite() {
--- tests/temperature_tests.rs
+++ tests/temperature_tests.rs
@@ -11,6 +11,7 @@ use TemperatureConverter::{
     DELISLE_FREEZING_TEMP,
     NEWTON_FREEZING_TEMP,
     REAUMUR_FREEZING_TEMP,
+    ROMER_FREEZING_TEMP,
 
     FAHRENHEIT_BOILING_TEMP,
     CELSIUS_BOILING_TEMP,
@@ -19,6 +20,7 @@ use TemperatureConverter::{
     DELISLE_BOILING_TEMP,
     NEWTON_BOILING_TEMP,
     REAUMUR_BOILING_TEMP,
+    ROMER_BOILING_TEMP,
 };
 
 const EPSILON: f64 = 1.0e-12;
@@ -27,11 +29,17 @@ fn approximately_equal(a: f64, b: f64, epsilon: f64) -> bool {
     (a - b).abs() <= epsilon
 }
 
-// Freezing point conversions
+//
+//
+// Freezing point conversion tests
+//
+//
 
 #[test]
 fn test_fahrenheit_to_other_freezing_point_conversions() {
+    // Freezing point in Fahrenheit
     let fahrenheit = Temperature::new(FAHRENHEIT_FREEZING_TEMP, TemperatureUnit::Fahrenheit);
+    
     let celsius = fahrenheit.convert_to(TemperatureUnit::Celsius);
     assert!(approximately_equal(celsius.value, CELSIUS_FREEZING_TEMP, EPSILON));
 
@@ -49,11 +57,16 @@ fn test_fahrenheit_to_other_freezing_point_conversions() {
    
    let reaumur = fahrenheit.convert_to(TemperatureUnit::Reaumur);
    assert!(approximately_equal(reaumur.value, REAUMUR_FREEZING_TEMP, EPSILON));
+
+   let romer = fahrenheit.convert_to(TemperatureUnit::Romer);
+   assert!(approximately_equal(romer.value,ROMER_FREEZING_TEMP, EPSILON));
 }
 
 #[test]
 fn test_celsius_to_other_freezing_point_conversions() {
+    // Freezing point in Celsius
     let celsius = Temperature::new(CELSIUS_FREEZING_TEMP, TemperatureUnit::Celsius);
+
     let fahrenheit = celsius.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_FREEZING_TEMP, EPSILON));
 
@@ -71,11 +84,16 @@ fn test_celsius_to_other_freezing_point_conversions() {
    
     let reaumur = celsius.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_FREEZING_TEMP, EPSILON));
+ 
+    let romer = celsius.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value,ROMER_FREEZING_TEMP, EPSILON));
  }
 
 #[test]
 fn test_kelvin_to_other_freezing_point_conversions() {
+    // Freezing point in Kelvin
     let kelvin = Temperature::new(KELVIN_FREEZING_TEMP, TemperatureUnit::Kelvin);
+
     let fahrenheit = kelvin.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_FREEZING_TEMP, EPSILON));
 
@@ -93,11 +111,16 @@ fn test_kelvin_to_other_freezing_point_conversions() {
    
     let reaumur = kelvin.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_FREEZING_TEMP, EPSILON));
+ 
+    let romer = kelvin.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value,ROMER_FREEZING_TEMP, EPSILON));
  }
 
 #[test]
 fn test_rankine_to_other_freezing_point_conversions() {
+    // Freezing point in Rankine
     let rankine = Temperature::new(RANKINE_FREEZING_TEMP, TemperatureUnit::Rankine);
+
     let fahrenheit = rankine.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_FREEZING_TEMP, EPSILON));
 
@@ -115,11 +138,16 @@ fn test_rankine_to_other_freezing_point_conversions() {
    
     let reaumur = rankine.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_FREEZING_TEMP, EPSILON));
+ 
+    let romer = rankine.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value,ROMER_FREEZING_TEMP, EPSILON));
  }
 
 #[test]
 fn test_delisle_to_other_freezing_point_conversions() {
+    // Freezing point in Delisle
     let delisle = Temperature::new(DELISLE_FREEZING_TEMP, TemperatureUnit::Delisle);
+
     let fahrenheit = delisle.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_FREEZING_TEMP, EPSILON));
 
@@ -137,11 +165,16 @@ fn test_delisle_to_other_freezing_point_conversions() {
    
     let reaumur = delisle.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_FREEZING_TEMP, EPSILON));
+ 
+    let romer = delisle.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value,ROMER_FREEZING_TEMP, EPSILON));
  }
 
 #[test]
 fn test_newton_to_other_freezing_point_conversions() {
+    // Freezing point in Newton
     let newton = Temperature::new(NEWTON_FREEZING_TEMP, TemperatureUnit::Newton);
+
     let fahrenheit = newton.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_FREEZING_TEMP, EPSILON));
 
@@ -159,10 +192,14 @@ fn test_newton_to_other_freezing_point_conversions() {
    
     let reaumur = newton.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_FREEZING_TEMP, EPSILON));
+ 
+    let romer = newton.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value,ROMER_FREEZING_TEMP, EPSILON));
  }
 
  #[test]
  fn test_reaumur_to_other_freezing_point_conversions() {
+    // Freezing point in Reaumur  
     let reaumur = Temperature::new(REAUMUR_FREEZING_TEMP, TemperatureUnit::Reaumur);
     
     let fahrenheit = reaumur.convert_to(TemperatureUnit::Fahrenheit);
@@ -182,13 +219,48 @@ fn test_newton_to_other_freezing_point_conversions() {
    
     let newton = reaumur.convert_to(TemperatureUnit::Newton);
     assert!(approximately_equal(newton.value, NEWTON_FREEZING_TEMP, EPSILON));
-}
 
-// Boiling point conversions
+    let romer = reaumur.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value,ROMER_FREEZING_TEMP, EPSILON));
+ }
+
+ #[test]
+fn test_romer_to_other_freezing_point_conversions() {
+    // Freezing point in Romer
+    let romer = Temperature::new(ROMER_FREEZING_TEMP, TemperatureUnit::Romer);
+
+    let fahrenheit = romer.convert_to(TemperatureUnit::Fahrenheit);
+    assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_FREEZING_TEMP, EPSILON));
+
+    let celsius = romer.convert_to(TemperatureUnit::Celsius);
+    assert!(approximately_equal(celsius.value, CELSIUS_FREEZING_TEMP, EPSILON));
+
+    let kelvin = romer.convert_to(TemperatureUnit::Kelvin);
+    assert!(approximately_equal(kelvin.value, KELVIN_FREEZING_TEMP, EPSILON));
+
+    let rankine = romer.convert_to(TemperatureUnit::Rankine);
+    assert!(approximately_equal(rankine.value, RANKINE_FREEZING_TEMP, EPSILON));
+
+    let delisle = romer.convert_to(TemperatureUnit::Delisle);
+    assert!(approximately_equal(delisle.value, DELISLE_FREEZING_TEMP, EPSILON));
+
+    let newton = romer.convert_to(TemperatureUnit::Newton);
+    assert!(approximately_equal(newton.value, NEWTON_FREEZING_TEMP, EPSILON));
+
+    let reaumur = romer.convert_to(TemperatureUnit::Reaumur);
+    assert!(approximately_equal(reaumur.value, REAUMUR_FREEZING_TEMP, EPSILON));
+}
+//
+//
+// Boiling point conversion tests
+//
+//
 
 #[test]
 fn test_fahrenheit_to_other_boiling_point_conversions() {
+    // Boiling point in Fahrenheit
     let fahrenheit = Temperature::new(FAHRENHEIT_BOILING_TEMP, TemperatureUnit::Fahrenheit);
+
     let celsius = fahrenheit.convert_to(TemperatureUnit::Celsius);
     assert!(approximately_equal(celsius.value, CELSIUS_BOILING_TEMP, EPSILON));
 
@@ -206,11 +278,16 @@ fn test_fahrenheit_to_other_boiling_point_conversions() {
     
     let reaumur = fahrenheit.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_BOILING_TEMP, EPSILON));
+    
+    let romer = fahrenheit.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value, ROMER_BOILING_TEMP, EPSILON));
 }
 
 #[test]
 fn test_celsius_to_other_boiling_point_conversions() {
+    // Boiling point in Celsius
     let celsius = Temperature::new(CELSIUS_BOILING_TEMP, TemperatureUnit::Celsius);
+
     let fahrenheit = celsius.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_BOILING_TEMP, EPSILON));
 
@@ -228,11 +305,16 @@ fn test_celsius_to_other_boiling_point_conversions() {
     
     let reaumur = celsius.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_BOILING_TEMP, EPSILON));
+    
+    let romer = celsius.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value, ROMER_BOILING_TEMP, EPSILON));
 }
 
 #[test]
 fn test_kelvin_to_other_boiling_point_conversions() {
+    // Boiling point in Kelvin
     let kelvin = Temperature::new(KELVIN_BOILING_TEMP, TemperatureUnit::Kelvin);
+
     let fahrenheit = kelvin.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_BOILING_TEMP, EPSILON));
 
@@ -250,11 +332,16 @@ fn test_kelvin_to_other_boiling_point_conversions() {
     
     let reaumur = kelvin.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_BOILING_TEMP, EPSILON));
+    
+    let romer = kelvin.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value, ROMER_BOILING_TEMP, EPSILON));
 }
 
 #[test]
 fn test_rankine_to_other_boiling_point_conversions() {
+    // Boiling point in Rankine
     let rankine = Temperature::new(RANKINE_BOILING_TEMP, TemperatureUnit::Rankine);
+
     let fahrenheit = rankine.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_BOILING_TEMP, EPSILON));
 
@@ -272,11 +359,16 @@ fn test_rankine_to_other_boiling_point_conversions() {
     
     let reaumur = rankine.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_BOILING_TEMP, EPSILON));
+    
+    let romer = rankine.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value, ROMER_BOILING_TEMP, EPSILON));
 }
 
 #[test]
 fn test_delisle_to_other_boiling_point_conversions() {
+    // Boiling point in Delisle
     let delisle = Temperature::new(DELISLE_BOILING_TEMP, TemperatureUnit::Delisle);
+
     let fahrenheit = delisle.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_BOILING_TEMP, EPSILON));
 
@@ -294,11 +386,16 @@ fn test_delisle_to_other_boiling_point_conversions() {
     
     let reaumur = delisle.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_BOILING_TEMP, EPSILON));
+    
+    let romer = delisle.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value, ROMER_BOILING_TEMP, EPSILON));
 }
 
 #[test]
 fn test_newton_to_other_boiling_point_conversions() {
+    // Boiling point in Newton
     let newton = Temperature::new(NEWTON_BOILING_TEMP, TemperatureUnit::Newton);
+
     let fahrenheit = newton.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_BOILING_TEMP, EPSILON));
 
@@ -316,11 +413,16 @@ fn test_newton_to_other_boiling_point_conversions() {
     
     let reaumur = newton.convert_to(TemperatureUnit::Reaumur);
     assert!(approximately_equal(reaumur.value, REAUMUR_BOILING_TEMP, EPSILON));
+    
+    let romer = newton.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value, ROMER_BOILING_TEMP, EPSILON));
 }
 
 #[test]
 fn test_reaumur_to_other_boiling_point_conversions() {
-    let reaumur = Temperature::new(REAUMUR_BOILING_TEMP, TemperatureUnit::Reaumur);
+    // Boiling point in Reaumur
+    let reaumur = Temperature::new(REAUMUR_BOILING_TEMP, TemperatureUnit::Reaumur); 
+
     let fahrenheit = reaumur.convert_to(TemperatureUnit::Fahrenheit);
     assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_BOILING_TEMP, EPSILON));
 
@@ -339,4 +441,34 @@ fn test_reaumur_to_other_boiling_point_conversions() {
   
     let newton = reaumur.convert_to(TemperatureUnit::Newton);
     assert!(approximately_equal(newton.value, NEWTON_BOILING_TEMP, EPSILON));
+    
+    let romer = reaumur.convert_to(TemperatureUnit::Romer);
+    assert!(approximately_equal(romer.value, ROMER_BOILING_TEMP, EPSILON));
+}
+
+#[test]
+fn test_romer_to_other_boiling_point_conversions() {
+    // Boiling point in Romer
+    let romer = Temperature::new(ROMER_BOILING_TEMP, TemperatureUnit::Romer);
+
+    let fahrenheit = romer.convert_to(TemperatureUnit::Fahrenheit);
+    assert!(approximately_equal(fahrenheit.value, FAHRENHEIT_BOILING_TEMP, EPSILON));
+
+    let celsius = romer.convert_to(TemperatureUnit::Celsius);
+    assert!(approximately_equal(celsius.value, CELSIUS_BOILING_TEMP, EPSILON));
+
+    let kelvin = romer.convert_to(TemperatureUnit::Kelvin);
+    assert!(approximately_equal(kelvin.value, KELVIN_BOILING_TEMP, EPSILON));
+
+    let rankine = romer.convert_to(TemperatureUnit::Rankine);
+    assert!(approximately_equal(rankine.value, RANKINE_BOILING_TEMP, EPSILON));
+
+    let delisle = romer.convert_to(TemperatureUnit::Delisle);
+    assert!(approximately_equal(delisle.value, DELISLE_BOILING_TEMP, EPSILON));
+
+    let newton = romer.convert_to(TemperatureUnit::Newton);
+    assert!(approximately_equal(newton.value, NEWTON_BOILING_TEMP, EPSILON));
+
+    let reaumur = romer.convert_to(TemperatureUnit::Reaumur);
+    assert!(approximately_equal(reaumur.value, REAUMUR_BOILING_TEMP, EPSILON));
 }
